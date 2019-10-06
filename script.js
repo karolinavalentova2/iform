f1331fa66f635db89bda0f5e5fe7e2c5d1c9b134
karolinakv <karolinavalentova2@gmail.com>
2019-10-07 01:04:59 +0200

    added log in page

---
+++ script.js
@@ -0,0 +1,10 @@
+function doLogin(e) {
+    e.preventDefault();
+    const userInputElement = document.getElementById('entryName');
+    const passwordInputElement = document.getElementById('entryPassword');
+
+    if(userInputElement.value === 'CryBabies' && passwordInputElement.value === 'password123'){
+        window.location.href = `${window.location.origin}/index.html`
+    } else alert('Wrong username or password!')
+}
+
