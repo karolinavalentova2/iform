2dc936bb9ab6f17e2cdeb44ad6bc42156561bb19
SEJ <sej@needrent.dk>
2019-10-07 15:01:55 +0200

    CSS changes and basket.html

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+// login
 function doLogin(e) {
-    e.preventDefault();
-    const userInputElement = document.getElementById('entryName');
-    const passwordInputElement = document.getElementById('entryPassword');
+  e.preventDefault();
+  const userInputElement = document.getElementById("entryName");
+  const passwordInputElement = document.getElementById("entryPassword");
 
-    if(userInputElement.value === 'CryBabies' && passwordInputElement.value === 'password123'){
-        window.location.href = `${window.location.origin}/index.html`
-    } else alert('Wrong username or password!')
+  if (
+    userInputElement.value === "CryBabies" &&
+    passwordInputElement.value === "password123"
+  ) {
+    window.location.href = `${window.location.origin}/index.html`;
+  } else alert("Wrong username or password!");
 }
+// Index
+// go to next page
+document.querySelector(".button-order-now").addEventListener("click", e => {
+  window.location.href = "basket.html";
+});
 
+//basket
