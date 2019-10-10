1203fab2ef00d21f63debed69a10fe0aeda97a9f
SEJ <sej@needrent.dk>
2019-10-10 15:01:59 +0200

    made a quick validation on the delivery form

--- javascript/delivery.js
+++ javascript/delivery.js
@@ -13,11 +13,34 @@ console.log(form);
 
 form.btnPayment.addEventListener("click", e => {
   e.preventDefault();
-  window.location.href = `payment.html?${listInformation[1]}?${
-    listInformation[2]
-  }?firstname=${form.firstname.value}?lastname=${form.lastname.value}?adress=${
-    form.adress.value
-  }?zip=${form.zip.value}?city=${form.city.value}?email=${
-    form.email.value
-  }?phone=${form.phone.value}`;
+  if (form.firstname.value == "") {
+    alert("You are missing your first name");
+    form.firstname.focus();
+  } else if (form.lastname.value == "") {
+    alert("You are missing your last name");
+    form.lastname.focus();
+  } else if (form.adress.value == "") {
+    alert("You are missing your adress");
+    form.adress.focus();
+  } else if (form.zip.value == "") {
+    alert("You are missing your ZIP code");
+    form.zip.focus();
+  } else if (form.city.value == "") {
+    alert("You are missing the city");
+    form.city.focus();
+  } else if (form.email.value == "") {
+    alert("You are missing your email");
+    form.email.focus();
+  } else if (form.phone.value == "") {
+    alert("You are missing a phone number");
+    form.phone.focus();
+  } else {
+    window.location.href = `payment.html?${listInformation[1]}?${
+      listInformation[2]
+    }?firstname=${form.firstname.value}?lastname=${
+      form.lastname.value
+    }?adress=${form.adress.value}?zip=${form.zip.value}?city=${
+      form.city.value
+    }?email=${form.email.value}?phone=${form.phone.value}`;
+  }
 });
